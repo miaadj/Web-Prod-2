455817ed47b7588959b19d5507cb624adf7b1f7c
miaadj <112648768+miaadj@users.noreply.github.com>
2022-09-08 11:24:57 -0400

    Add files via upload

---
+++ daynight.js
@@ -0,0 +1,11 @@
+let hour = new Date().getHours();
+   console.log(hour);
+
+let background = document.querySelector('body'); 
+   if (hour >= 18 || hour < 6) {
+      
+      background.style.backgroundColor = '#0c376f';
+     } else {
+      background.style.backgroundColor = '#f4f0c3'; 
+  }  
+
